274e822eb9947e2b6550d926a9e48943c191098b
ManuelR11 <rod21509@uvg.edu.gt>
2024-08-06 20:01:27 -0600

    inicio de pagina

---
+++ cliente_xmpp/src/Chat/chat.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import './chat.css';
+
+const Chat = () => {
+  const adjustScreenSize = () => {
+    const screen = document.getElementById('computer-screen');
+    const screenWidth = window.innerWidth * 0.9;
+    const screenHeight = window.innerHeight * 0.7;
+
+    screen.style.width = `${screenWidth}px`;
+    screen.style.height = `${screenHeight}px`;
+
+    if (screenWidth > 1200) {
+      screen.style.width = '1000px';
+    }
+
+    if (screenHeight > 800) {
+      screen.style.height = '800px';
+    }
+
+    if (window.innerWidth <= 600) {
+      screen.style.width = `${window.innerWidth * 0.95}px`;
+      screen.style.height = `${window.innerHeight * 0.8}px`;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', adjustScreenSize);
+    adjustScreenSize();
+    return () => {
+      window.removeEventListener('resize', adjustScreenSize);
+    };
+  }, []);
+
+  return (
+    <div id="computer-screen">
+      {/* Contenido de la pantalla */}
+    </div>
+  );
+};
+
+export default Chat;
